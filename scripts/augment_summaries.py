14eacac182dab0d31fe22b82b8f7e560ac09d5d3
Eyal Mazuz <mazuzeyal@protonmail.com>
2024-12-16 10:02:48 +0200

    WIP: improved saying the augmented data, added multi-processing and make the code clear

--- scripts/augment_summaries.py
+++ scripts/augment_summaries.py
@@ -1,4 +1,12 @@
 import argparse
+import os
+import tomllib
+from collections.abc import Iterator
+from concurrent.futures import ProcessPoolExecutor
+from functools import partial
+from itertools import chain
+
+import pandas as pd
 
 from src.augmentations import get_augmentations
 from src.utils import extract_texts, load_data
@@ -13,11 +21,11 @@ def parse_args() -> argparse.Namespace:
         type=str,
         nargs="+",
         required=True,
-        choices=["sentence-removal", "middle-shuffle"],
+        choices=["sentence-removal", "sentence-shuffle"],
         help="Which type of augmentations to use on the texts",
     )
     parser.add_argument(
-        "--augmentations-config",
+        "--augmentations-config-path",
         type=str,
         help="Config file for augmentation to change the default behaviour",
     )
@@ -25,7 +33,29 @@ def parse_args() -> argparse.Namespace:
         "--max-examples", type=int, default=-1, help="The maximum number of examples to generate augmentatios for"
     )
 
+    parser.add_argument(
+        "--num-processes", type=int, default=-1, help="The number of threads to use when running augmentations"
+    )
+
     return parser.parse_args()
+
+
+def augment_text(text: str, augmentation) -> dict[str, str] | None:
+    augmented_text = augmentation(text)
+    if augmented_text is not None:
+        return {"original-text": text, "augmented-text": augmented_text, "augmentation": str(augmentation)}
+    else:
+        return None
+
+
+def augment_text_multiple(text: str, augmentations) -> list[dict[str, str]]:
+    augmented_texts: list[dict[str, str]] = []
+    for augmentation in augmentations:
+        augmneted_text = augment_text(text, augmentation)
+        if augmneted_text is not None:
+            augmented_texts.append(augmneted_text)
+
+    return augmented_texts
 
 
 def main(args: argparse.Namespace) -> None:
@@ -33,17 +63,29 @@ def main(args: argparse.Namespace) -> None:
     texts = extract_texts(summaries=summaries, only_summaries=True, use_ai_summaries=False)
     if args.max_examples != -1:
         texts = texts[: args.max_examples]
-    augmentations = get_augmentations(args.augmentations, args.augmentations_config)
 
-    augmented_texts: list[str] = []
-    for text in texts:
-        for augmentation in augmentations:
-            augmented_text = augmentation(text)
-            if augmented_text is not None:
-                augmented_texts.append(augmented_text)
+    augmentations_config = None
+    if args.augmentations_config_path is not None:
+        with open(args.augmentations_config_path, "rb") as fd:
+            augmentations_config = tomllib.load(fd)
 
-    with open(args.output_path, "w") as fd:
-        fd.write("\n\n\n".join(augmented_texts))  # TODO: change the way texts are saved to the disk
+    augmentations = get_augmentations(args.augmentations, augmentations_config)
+
+    augment_text_partial = partial(augment_text_multiple, augmentations=augmentations)
+
+    results: Iterator[list[dict[str, str]]]
+    if args.num_processes == -1:
+        results = map(augment_text_partial, texts)
+
+    else:
+        print("Running multi-processing")
+        with ProcessPoolExecutor(max_workers=args.num_processes) as pool:
+            results = pool.map(augment_text_partial, texts, chunksize=10)
+
+    df = pd.DataFrame(chain.from_iterable(results), columns=["original-text", "augmented-text", "augmentation"])
+    if not os.path.exists(args.output_path):
+        os.makedirs(args.output_path, exist_ok=True)
+    df.to_csv(os.path.join(args.output_path, "augmented_texts.csv"), index=False)
 
 
 if __name__ == "__main__":
--- src/augmentations.py
+++ src/augmentations.py
@@ -10,7 +10,7 @@ class Augmentation(ABC):
 
 
 class SentenceRemoval(Augmentation):
-    def __init__(self, start: int = 1, end: int = 1, k: int = 2, min_sentence_length: int = 10) -> None:
+    def __init__(self, start: int = 1, end: int = 1, k: int = 3, min_sentence_length: int = 10) -> None:
         self.start = start
         self.end = end
         self.k = k
@@ -32,14 +32,49 @@ class SentenceRemoval(Augmentation):
         augmented_sentences = [sentence for i, sentence in enumerate(sentences) if i not in indices_to_remove]
         return ". ".join(augmented_sentences) + "."
 
+    def __str__(self) -> str:
+        return "sentence-removal"
+
+
+class SetenceShuffle(Augmentation):
+    def __init__(self, window_size: int = 3, min_sentence_length: int = 10) -> None:
+        self.window_size = window_size
+        self.min_sentence_length = min_sentence_length
+
+        assert (self.window_size * 2 + 1) < min_sentence_length - 2, (
+            f"Can't create augmentor that shuffles {self.window_size * 2 + 1} sentences"
+            f"for texts with minimum {self.min_sentence_length} sentences long"
+            f"with window size = {self.window_size}"
+        )
+
+    def __call__(self, text: str) -> str | None:
+        if text is None:
+            return None
+        sentences = [sentence.strip() for sentence in text.strip().split(".") if sentence.strip()]
+        if len(sentences) < self.min_sentence_length:
+            return None
+        middle = len(sentences) // 2
+        shuffle_block = sentences[middle - self.window_size : middle + self.window_size]
+        random.shuffle(shuffle_block)
+        return (
+            ". ".join(sentences[: middle - self.window_size] + shuffle_block + sentences[middle + self.window_size :])
+            + "."
+        )
+
+    def __str__(self) -> str:
+        return "sentence-shuffle"
+
 
 def get_augmentations(augs_type: list[str], augmentations_config: dict[str, Any] | None) -> list[Augmentation]:
     augmentations: list[Augmentation] = []
     for type_ in augs_type:
         config = augmentations_config.get(type_, {}) if augmentations_config is not None else {}
+        augmentation: Augmentation
         match type_:
             case "sentence-removal":
                 augmentation = SentenceRemoval(**config)
+            case "sentence-shuffle":
+                augmentation = SetenceShuffle(**config)
             case _:
                 raise ValueError(f"{type_} is not a valid augmentation")
         augmentations.append(augmentation)
