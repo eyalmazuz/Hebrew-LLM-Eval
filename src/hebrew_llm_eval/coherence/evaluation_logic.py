a061ac80a05f59688274dbc1bbed6a5b043a266c
Eyal Mazuz <mazuzeyal@protonmail.com>
2025-04-08 15:12:26 +0300

    Added pair-wise ranking accuracy

--- src/hebrew_llm_eval/coherence/evaluation_logic.py
+++ src/hebrew_llm_eval/coherence/evaluation_logic.py
@@ -29,8 +29,10 @@ def ranking_eval(
         )
     model.eval()
 
-    total = 0
-    correct = 0
+    top_correct = 0
+    top_total = 0
+    pair_correct = 0
+    pair_total = 0
     for i in trange(len(test_dataset)):
         encodings, len_shuffled = test_dataset[i]
         encodings = {k: v.to(device) for k, v in encodings.items()}
@@ -39,15 +41,19 @@ def ranking_eval(
         if len_shuffled == 0:
             print("  --> Skipping this item for ranking (cannot shuffle)")
         else:
-            print("  --> Evaluating this item...")
+            # print("  --> Evaluating this item...")
             with torch.no_grad():
                 outputs = model(**encodings)
                 probs = torch.softmax(outputs.logits, dim=1)[:, 1]
-                is_success = torch.max(probs) == probs[0]
-                total += 1
-                if is_success:
-                    correct += 1
-    print(f"Ranking accuracy = {correct / total:.3f}")
+
+                top_correct += int(torch.max(probs) == probs[0])
+                pair_correct += int(torch.sum(probs[0] > probs[1:]))
+                pair_total += len_shuffled
+                top_total += 1
+
+    print(f"Top ranking accuracy = {top_correct / top_total:.3f}")
+    print(f"Pair ranking accuracy = {pair_correct / pair_total:.3f}")
 
     if wandb is not None:
-        wandb.summary["ranking_accuracy"] = correct / total  # type: ignore
+        wandb.summary["top_ranking_accuracy"] = top_correct / top_total  # type: ignore
+        wandb.summary["pair_ranking_accuracy"] = pair_correct / pair_total  # type: ignore
